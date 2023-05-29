99bb0243436bb54446735fa3e8ad781923724edc
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2023-05-29 17:25:50 -0300

    My first steps with php

---
+++ ex000/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>me livrando da maldição</title>
+</head>
+<body>
+    <h1>
+        <?php 
+        echo "Olá, mundo! 🌍";/* ou utilizo a tag 
+        \u{código UTF} para add emojis*/
+        ?>
+    </h1>
+    <p>Vamo tentar nos livrar da maldição Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt consequatur corporis ipsam nisi, est minima autem optio omnis. Quo officiis eum nulla veniam? Ipsa, velit. Iste nihil eum praesentium neque?</p>
+</body>
+</html> 
